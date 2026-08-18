c920b0a92e904d5de2209ba0e761bb200ffe0168
EkanshTyagi7 <ekanshtyagi107@gmail.com>
2026-08-18 23:30:45 +0530

    add upload form functionality

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -6,11 +6,26 @@ const upload = () => {
   const [isProcessing, setIsProcessing] = useState(false);
   const [statusText, setStatusText] = useState("");
   const [file, setFile] = useState<File | null>(null);
-  
 
-  const handleFileSelect=(file: File | null)=>{ setFile(file)};
+  const handleFileSelect = (file: File | null) => {
+    setFile(file);
+  };
 
-  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {};
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget.closest("form");
+    if (!form) return;
+    const formData = new FormData(form);
+    const companyName = formData.get("company-name");
+    const jobTitle = formData.get("job-title");
+    const jobDescription = formData.get("job-description");
+    console.log({
+      companyName,
+      jobTitle,
+      jobDescription,
+      file,
+    });
+  };
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
